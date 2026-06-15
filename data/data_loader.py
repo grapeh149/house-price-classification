9ba3bdc5c98421db9fcaba8cd9cfe64740309841
ThaiBao107 <nguyenbao1072004@gmail.com>
2026-06-15 17:47:47 +0700

    update

--- data/data_loader.py
+++ data/data_loader.py
@@ -2,14 +2,21 @@ import pandas as pd
 import os
 import glob
 
+# If you want to crawl manual data, you could use this method.
+# def load_data():
+#     current_path = os.getcwd()
+#     folder_path = pd.read_csv(f'{current_path}\\clean_data')
+#     csv_files = glob.glob(os.path.join(folder_path , "*.csv"))
+#     all_data = pd.concat([pd.read_csv(f) for f in csv_files], ignore_index=True)
+#     all_data.drop(columns=['Unnamed: 0'], inplace=True)
+#     print(all_data)
+#     return all_data
+
+# If you have clean datasets, you could use this method.
 def load_data():
-    current_path = os.getcwd()
-    folder_path = pd.read_csv(f'{current_path}\\clean_data')
-    csv_files = glob.glob(os.path.join(folder_path , "*.csv"))
-    all_data = pd.concat([pd.read_csv(f) for f in csv_files], ignore_index=True)
-    all_data.drop(columns=['Unnamed: 0'], inplace=True)
-    print(all_data)
-    return all_data
+    data = pd.read_csv('data.csv')
+    data = pd.read_csv('data.csv', sep=',', encoding='utf-8',  index_col=0  )
+    return data
 
 
 
